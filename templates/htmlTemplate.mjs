b02cbdb8ff7d2b84417dc82df5a09ffeefea8071
Scott Rouse <scott.rouse@gmail.com>
2023-03-05 12:26:38 -0600

    trying netlify

--- templates/htmlTemplate.mjs
+++ templates/htmlTemplate.mjs
@@ -5,7 +5,7 @@ export default function renderHTML(content, css) {
 <html>
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <link rel="stylesheet" href="https://6404dc8fa4dd67285cf0e9bb--admirable-pegasus-a42d6b.netlify.app/transformations/fdst-web/rds.css" />
+    <link rel="stylesheet" href="https://admirable-pegasus-a42d6b.netlify.app/transformations/fdst-web/rds.css" />
     <style>
       ${css}
     </style>
