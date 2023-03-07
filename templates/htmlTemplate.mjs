214da1df723bca87ed94af5d45fa53f5fc6490ea
Scott Rouse <scott.rouse@gmail.com>
2023-03-07 15:48:05 -0600

    updating url for readme-design-system

--- templates/htmlTemplate.mjs
+++ templates/htmlTemplate.mjs
@@ -5,8 +5,8 @@ export default function renderHTML(content, css) {
 <html>
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <link rel="stylesheet" href="https://admirable-pegasus-a42d6b.netlify.app/transformations/fds-web/rds.css" />
-    <script src="https://admirable-pegasus-a42d6b.netlify.app/transformations/fds-web/icons/fds-widget-web-comp.js"></script>
+    <link rel="stylesheet" href="https://readme-design-system.netlify.app/transformations/fds-web/rds.css" />
+    <script src="https://readme-design-system.netlify.app/transformations/fds-web/icons/fds-widget-web-comp.js"></script>
     <style>
       ${css}
     </style>
