98cd6c045a84a6057a60fc56533fe6d6e9cd9aa0
Scott Rouse <scott.rouse@gmail.com>
2023-04-22 14:57:04 -0500

    - adding version api in order to keep widget synced with latest version

---
+++ netlify/functions/version.ts
@@ -0,0 +1,25 @@
+import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+
+const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+      },
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ version: 'v0.0.5' }),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+  };
+};
+
+export { handler };
